8b43a8c52ae8e1b17b28837bc38f487861a0b288
zarishbilal <zarishsuleman@gmail.com>
2025-01-20 11:05:39 -0700

    added title

--- App.tsx
+++ App.tsx
@@ -5,3 +5,14 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Feather from '@expo/vector-icons/Feather';
 import Entypo from '@expo/vector-icons/Entypo';
 import Ionicons from '@expo/vector-icons/Ionicons';
+
+<View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.row}>
+      <AntDesign name="left" size={24} color="black" style={styles.iconLeft}/>
+      <View style={styles.textContainer}>
+        <Text style={styles.saitText}>SAIT</Text>
+        <Text style={styles.collegeText}>College and University</Text>
+      </View>
+        <FontAwesome name="plus-square-o" size={24} color="black" />
+</View>
